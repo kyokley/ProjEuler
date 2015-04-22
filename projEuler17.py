05a515bbdb5636915e7df29b415d7adee4a85ba1
Kevin Yokley <kyokley2@gmail.com>
2015-04-22 09:19:27 -0500

    Fixing tabbing

--- projEuler17.py
+++ projEuler17.py
@@ -4,38 +4,39 @@ import projEulerFuncs
 digitize = projEulerFuncs.digitize
 
 def ones(num):
-	vals = {0:"",1:"one",2:"two",3:"three",4:"four",5:"five",6:"six",7:"seven",8:"eight",9:"nine"}
-	return vals[num]
+    vals = {0:"",1:"one",2:"two",3:"three",4:"four",5:"five",6:"six",7:"seven",8:"eight",9:"nine"}
+    return vals[num]
 
 def tens(num):
-	digitLst = digitize(num)
-	vals = {10:"ten",11:"eleven",12:"twelve",13:"thirteen",14:"fourteen",15:"fifteen",16:"sixteen",17:"seventeen",18:"eighteen",19:"nineteen"}
+    digitLst = digitize(num)
+    vals = {10:"ten",11:"eleven",12:"twelve",13:"thirteen",14:"fourteen",15:"fifteen",16:"sixteen",17:"seventeen",18:"eighteen",19:"nineteen"}
 
-	vals2 = {2:"twenty",3:"thirty",4:"forty",5:"fifty",6:"sixty",7:"seventy",8:"eighty",9:"ninety"}
+    vals2 = {2:"twenty",3:"thirty",4:"forty",5:"fifty",6:"sixty",7:"seventy",8:"eighty",9:"ninety"}
 
-	if len(digitLst) == 1:
-		return ones(digitLst[0])
+    if len(digitLst) == 1:
+        return ones(digitLst[0])
 
-	if digitLst[0] == 1:
-		return vals[num]
+    if digitLst[0] == 1:
+        return vals[num]
 
-	return vals2[digitLst[0]] + ones(digitLst[1])	
+    return vals2[digitLst[0]] + ones(digitLst[1])
 
 def hundreds(num):
-	digitLst = digitize(num)
+    digitLst = digitize(num)
 
-	val = {1:"onehundredand",2:"twohundredand",3:"threehundredand",4:"fourhundredand",5:"fivehundredand",6:"sixhundredand",7:"sevenhundredand",8:"eighthundredand",9:"ninehundredand"}
+    val = {1:"onehundredand",2:"twohundredand",3:"threehundredand",4:"fourhundredand",5:"fivehundredand",6:"sixhundredand",7:"sevenhundredand",8:"eighthundredand",9:"ninehundredand"}
 
-	return val[digitLst[0]] + tens(num % 100)
+    return val[digitLst[0]] + tens(num % 100)
 
 def letters(num):
-	digitLst = digitize(num)
-	funcs = {1:ones,2:tens,3:hundreds}
-	
-	return funcs[len(digitLst)](num)
+    digitLst = digitize(num)
+    funcs = {1:ones,2:tens,3:hundreds}
+
+    return funcs[len(digitLst)](num)
 
 def main():
-	print "blah"
+    import pdb; pdb.set_trace()
+    letters(328)
 
 if __name__ == "__main__":
-	main()
+    main()
